5f0f05f7bf7be5a9f8cc4f3bcf697a93dffd67ab
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-05-11 15:31:53 +0200

    Add tests for the value class

---
+++ tests/value.cpp
@@ -0,0 +1,77 @@
+#include "catch/catch.hpp"
+#include "pacif.hpp"
+
+TEST_CASE("CIF values", "[]") {
+    SECTION("Missing") {
+        auto missing = pacif::value();
+        REQUIRE(missing.is_missing());
+
+        CHECK(missing.get_kind() == pacif::value::kind::missing);
+
+        CHECK_THROWS_AS(missing.as_real(), pacif::error);
+        CHECK_THROWS_AS(missing.as_string(), pacif::error);
+        CHECK_THROWS_AS(missing.as_map(), pacif::error);
+        CHECK_THROWS_AS(missing.as_vec(), pacif::error);
+    }
+
+    SECTION("Reals") {
+        auto real = pacif::value(42.0);
+        REQUIRE(real.is_real());
+        CHECK(real.as_real() == 42.0);
+
+        real = pacif::value(42.0f);
+        REQUIRE(real.is_real());
+        CHECK(real.as_real() == static_cast<pacif::real_t>(42.0f));
+
+        real = pacif::value(42);
+        REQUIRE(real.is_real());
+        CHECK(real.as_real() == static_cast<pacif::real_t>(42));
+
+        CHECK(real.get_kind() == pacif::value::kind::real);
+
+        CHECK_THROWS_AS(real.as_string(), pacif::error);
+        CHECK_THROWS_AS(real.as_map(), pacif::error);
+        CHECK_THROWS_AS(real.as_vec(), pacif::error);
+    }
+
+    SECTION("Strings") {
+        auto string = pacif::value("foobar");
+        REQUIRE(string.is_string());
+        CHECK(string.as_string() == "foobar");
+
+        CHECK(string.get_kind() == pacif::value::kind::string);
+
+        CHECK_THROWS_AS(string.as_real(), pacif::error);
+        CHECK_THROWS_AS(string.as_map(), pacif::error);
+        CHECK_THROWS_AS(string.as_vec(), pacif::error);
+    }
+
+    SECTION("Map") {
+        auto map = pacif::value(pacif::map_t {
+            {"first", pacif::value("foobar")},
+            {"second", pacif::value(22)},
+        });
+        REQUIRE(map.is_map());
+        CHECK(map.as_map().find("first")->second.as_string() == "foobar");
+        CHECK(map.as_map().find("second")->second.as_real() == 22);
+
+        CHECK(map.get_kind() == pacif::value::kind::map);
+
+        CHECK_THROWS_AS(map.as_string(), pacif::error);
+        CHECK_THROWS_AS(map.as_real(), pacif::error);
+        CHECK_THROWS_AS(map.as_vec(), pacif::error);
+    }
+
+    SECTION("Vec") {
+        auto vec = pacif::value(pacif::vec_t{pacif::value("foobar"), pacif::value(22)});
+        REQUIRE(vec.is_vec());
+        CHECK(vec.as_vec()[0].as_string() == "foobar");
+        CHECK(vec.as_vec()[1].as_real() == 22);
+
+        CHECK(vec.get_kind() == pacif::value::kind::vec);
+
+        CHECK_THROWS_AS(vec.as_string(), pacif::error);
+        CHECK_THROWS_AS(vec.as_real(), pacif::error);
+        CHECK_THROWS_AS(vec.as_map(), pacif::error);
+    }
+}
