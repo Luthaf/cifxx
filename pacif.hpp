177a72b6c23f11aa8b9dc1e0de5c686a71eb9407
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-08-25 15:41:29 +0200

    Implement tokenization

--- pacif.hpp
+++ pacif.hpp
@@ -31,6 +31,7 @@
 #include <memory>
 #include <string>
 #include <cassert>
+#include <algorithm>
 #include <exception>
 
 namespace pacif {
@@ -52,33 +53,6 @@ struct error: public std::runtime_error {
 
 /// Collection of functions to check conformance to the CIF grammar
 namespace parsing {
-    using it = string_t::const_iterator;
-
-    /// Check if a given char is an ordinary char
-    inline bool is_ordinary_char(char c) {
-        return c > 32 && c < 127 && c != '"' && c != '#' && c != '$' &&
-               c != '\'' && c != ';' && c != '_' && c != '[' && c != ']';
-    }
-
-    /// Check if a given char is a non whitespace printable char
-    inline bool is_non_blank_char(char c) {
-        return c > 32 && c < 127;
-    }
-
-    /// Check if the string between `begin` and `end` is a tag name.
-    ///
-    ///     <tag_name> = '_' {<non_blank_char>}+
-    inline bool is_tag_name(it begin, it end) {
-        if (end - begin < 2) return false;
-        if (begin[0] != '_') return false;
-        auto result = is_non_blank_char(*(begin++));
-        while (begin != end) {
-            result = result && is_non_blank_char(*begin);
-            begin++;
-        }
-        return result;
-    }
-
     /// Basic token in CIF grammar
     class token final {
     public:
@@ -250,6 +224,184 @@ namespace parsing {
             string_t string_;
             real_t real_;
         };
+    };
+
+    /// Check if a given char is an ordinary char
+    inline bool is_ordinary_char(char c) {
+        return c > 32 && c < 127 && c != '"' && c != '#' && c != '$' &&
+               c != '\'' && c != ';' && c != '_' && c != '[' && c != ']';
+    }
+
+    /// Check if a given char is a non whitespace printable char
+    inline bool is_non_blank_char(char c) {
+        return c > 32 && c < 127;
+    }
+
+    /// Check if a given char is a non whitespace printable char
+    inline bool is_whitespace(char c) {
+        return c == ' ' || c == '\t' || c == '\r' || c == '\n';
+    }
+
+    /// Check if a given char is a given digit
+    inline bool is_digit(char c) {
+        return '0' <= c && c <= '9';
+    }
+
+    /// Check if `name` is a CIF tag name.
+    ///
+    ///     <tag_name> = '_' {<non_blank_char>}+
+    inline bool is_tag_name(const std::string& name) {
+        if (name.length() < 2) return false;
+        if (name[0] != '_') return false;
+        auto begin = name.begin();
+        auto result = is_non_blank_char(*(begin++));
+        while (begin != name.end()) {
+            result = result && is_non_blank_char(*begin);
+            begin++;
+        }
+        return result;
+    }
+
+    class parser final {
+    public:
+        explicit parser(std::string input):
+            input_(std::move(input)), current_(input_.begin()), end_(input_.end()) {}
+
+        /// Yield the next token
+        token next() {
+            bool skipped_whitespace_and_comments = false;
+            while (!skipped_whitespace_and_comments) {
+                if (current_ == end_) { // did we reached the end of input?
+                    return token::eof();
+                } else if (is_whitespace(*current_)) { // skip whitespaces
+                    current_++;
+                    continue;
+                } else if (*current_ == '#') {  // skip comment as needed
+                    while (++current_ != end_ && *current_ != '\n' && *current_ != '\r') {
+                        // skip character. We don't check for windows style line
+                        // ending (\r\n), because any remaining \n will be skipped
+                        // later.
+                    }
+                    continue;
+                } else {
+                    skipped_whitespace_and_comments = true;
+                }
+            }
+
+            // Get the token content
+            std::string content;
+            if (*current_ == '\'') {  // single quoted string
+                while (++current_ != end_) {
+                    if (*current_ == '\'' && (current_ + 1 == end_ || is_whitespace(current_[1]))) {
+                        return token_for_value(content);
+                    } else {
+                        content.push_back(*current_);
+                    }
+                }
+            } else if (*current_ == '"') {  // double quoted string
+                while (++current_ != end_) {
+                    if (*current_ == '"' && (current_ + 1 == end_ || is_whitespace(current_[1]))) {
+                        return token_for_value(content);
+                    } else {
+                        content.push_back(*current_);
+                    }
+                }
+            } else if (*current_ == ';' && previous_was_eol()) {  // multilines string
+                while (++current_ != end_) {
+                    if (*current_ == ';' && previous_was_eol()) {
+                        return token_for_value(content);
+                    } else {
+                        content.push_back(*current_);
+                    }
+                }
+            } else {
+                // unquoted text or other values
+                content.push_back(*current_);
+                while (++current_ != end_) {
+                    if (is_non_blank_char(*current_)) {
+                        content.push_back(*current_);
+                    } else {
+                        // we found a whitespace
+                        break;
+                    }
+                }
+
+                // check for reserved words, we only need to do this with
+                // unquoted strings
+                auto beggining = content.substr(0, 5);
+                std::transform(beggining.begin(), beggining.end(), beggining.begin(), ::tolower);
+                if (beggining == "data_") {
+                    return token::data(content.substr(5));
+                } else if (beggining == "save_") {
+                    return token::save(content.substr(5));
+                } else if (beggining == "loop_") {
+                    return token::loop();
+                } else if (beggining == "stop_") {
+                    return token::stop();
+                } else if (beggining == "globa") {
+                    beggining = content;
+                    std::transform(beggining.begin(), beggining.end(), beggining.begin(), ::tolower);
+                    if (beggining == "global_") {
+                        return token::global();
+                    } else {
+                        return token::string(content);
+                    }
+                }
+                return token_for_value(content);
+            }
+            throw error("this should be unreachable");
+        }
+
+    private:
+        bool previous_was_eol() const {
+            assert(current_ > input_.begin());
+            auto c = current_[-1];
+            return c != '\n' || c != '\r';
+        }
+
+        token token_for_value(std::string content) const {
+            std::string number = content;
+            // check if we have the `number(precision)` form
+            if (content.length() >= 4) {
+                auto last = content.length() - 1;
+                if (content[last] == ')' && is_digit(content[last - 1]) && content[last - 2] == '(') {
+                    number = content.substr(0, last - 2) + content[last - 1];
+                }
+            }
+
+            // try to get an integer
+            try {
+                std::size_t processed = 0;
+                integer_t value = std::stoll(number, &processed);
+                if (processed == number.length()) {
+                    return token::integer(value);
+                }
+            } catch (const std::invalid_argument&) {
+                // do nothing, this is not an integer
+            } catch (const std::out_of_range&) {
+                throw error("integer value " + content + " is too big for 64-bit integer type");
+            }
+
+            // try to get a real
+            try {
+                std::size_t processed = 0;
+                real_t value = std::stod(number, &processed);
+                if (processed == number.length()) {
+                    return token::real(value);
+                }
+            } catch (const std::invalid_argument&) {
+                // do nothing, this is not a real
+            } catch (const std::out_of_range&) {
+                throw error("real value " + content + " is too big for 64-bit float type");
+            }
+
+            // default to a string value
+            return token::string(content);
+        }
+
+        const std::string input_;
+        string_t::const_iterator current_;
+        const string_t::const_iterator end_;
     };
 }
 
@@ -452,7 +604,7 @@ public:
     /// is pointing to the newly inserted data.
     template<class T>
     std::pair<iterator, bool> insert(std::string key, T val) {
-        if (!parsing::is_tag_name(key.begin(), key.end())) {
+        if (!parsing::is_tag_name(key)) {
             throw error(key + " is not a valid data tag name");
         }
         return data_.emplace(std::move(key), value(std::move(val)));
--- tests/parsing.cpp
+++ tests/parsing.cpp
@@ -1,193 +1,51 @@
 #include "catch/catch.hpp"
 #include "pacif.hpp"
 
-TEST_CASE("CIF parsing") {
+TEST_CASE("basic values parsing") {
+    SECTION("digit") {
+        const auto DIGITS = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+        };
+
+        for (auto c: DIGITS) {
+            CHECK(pacif::parsing::is_digit(c));
+        }
+    }
+
     SECTION("ordinary char") {
-        CHECK(pacif::parsing::is_ordinary_char('!'));
-        CHECK(pacif::parsing::is_ordinary_char('%'));
-        CHECK(pacif::parsing::is_ordinary_char('&'));
-        CHECK(pacif::parsing::is_ordinary_char('('));
-        CHECK(pacif::parsing::is_ordinary_char(')'));
-        CHECK(pacif::parsing::is_ordinary_char('*'));
-        CHECK(pacif::parsing::is_ordinary_char('+'));
-        CHECK(pacif::parsing::is_ordinary_char(','));
-        CHECK(pacif::parsing::is_ordinary_char('-'));
-        CHECK(pacif::parsing::is_ordinary_char('.'));
-        CHECK(pacif::parsing::is_ordinary_char('/'));
-        CHECK(pacif::parsing::is_ordinary_char('0'));
-        CHECK(pacif::parsing::is_ordinary_char('1'));
-        CHECK(pacif::parsing::is_ordinary_char('2'));
-        CHECK(pacif::parsing::is_ordinary_char('3'));
-        CHECK(pacif::parsing::is_ordinary_char('4'));
-        CHECK(pacif::parsing::is_ordinary_char('5'));
-        CHECK(pacif::parsing::is_ordinary_char('6'));
-        CHECK(pacif::parsing::is_ordinary_char('7'));
-        CHECK(pacif::parsing::is_ordinary_char('8'));
-        CHECK(pacif::parsing::is_ordinary_char('9'));
-        CHECK(pacif::parsing::is_ordinary_char(':'));
-        CHECK(pacif::parsing::is_ordinary_char('<'));
-        CHECK(pacif::parsing::is_ordinary_char('='));
-        CHECK(pacif::parsing::is_ordinary_char('>'));
-        CHECK(pacif::parsing::is_ordinary_char('?'));
-        CHECK(pacif::parsing::is_ordinary_char('@'));
-        CHECK(pacif::parsing::is_ordinary_char('A'));
-        CHECK(pacif::parsing::is_ordinary_char('B'));
-        CHECK(pacif::parsing::is_ordinary_char('C'));
-        CHECK(pacif::parsing::is_ordinary_char('D'));
-        CHECK(pacif::parsing::is_ordinary_char('E'));
-        CHECK(pacif::parsing::is_ordinary_char('F'));
-        CHECK(pacif::parsing::is_ordinary_char('G'));
-        CHECK(pacif::parsing::is_ordinary_char('H'));
-        CHECK(pacif::parsing::is_ordinary_char('I'));
-        CHECK(pacif::parsing::is_ordinary_char('J'));
-        CHECK(pacif::parsing::is_ordinary_char('K'));
-        CHECK(pacif::parsing::is_ordinary_char('L'));
-        CHECK(pacif::parsing::is_ordinary_char('M'));
-        CHECK(pacif::parsing::is_ordinary_char('N'));
-        CHECK(pacif::parsing::is_ordinary_char('O'));
-        CHECK(pacif::parsing::is_ordinary_char('P'));
-        CHECK(pacif::parsing::is_ordinary_char('Q'));
-        CHECK(pacif::parsing::is_ordinary_char('R'));
-        CHECK(pacif::parsing::is_ordinary_char('S'));
-        CHECK(pacif::parsing::is_ordinary_char('T'));
-        CHECK(pacif::parsing::is_ordinary_char('U'));
-        CHECK(pacif::parsing::is_ordinary_char('V'));
-        CHECK(pacif::parsing::is_ordinary_char('W'));
-        CHECK(pacif::parsing::is_ordinary_char('X'));
-        CHECK(pacif::parsing::is_ordinary_char('Y'));
-        CHECK(pacif::parsing::is_ordinary_char('Z'));
-        CHECK(pacif::parsing::is_ordinary_char('\\'));
-        CHECK(pacif::parsing::is_ordinary_char('^'));
-        CHECK(pacif::parsing::is_ordinary_char('`'));
-        CHECK(pacif::parsing::is_ordinary_char('a'));
-        CHECK(pacif::parsing::is_ordinary_char('b'));
-        CHECK(pacif::parsing::is_ordinary_char('c'));
-        CHECK(pacif::parsing::is_ordinary_char('d'));
-        CHECK(pacif::parsing::is_ordinary_char('e'));
-        CHECK(pacif::parsing::is_ordinary_char('f'));
-        CHECK(pacif::parsing::is_ordinary_char('g'));
-        CHECK(pacif::parsing::is_ordinary_char('h'));
-        CHECK(pacif::parsing::is_ordinary_char('i'));
-        CHECK(pacif::parsing::is_ordinary_char('j'));
-        CHECK(pacif::parsing::is_ordinary_char('k'));
-        CHECK(pacif::parsing::is_ordinary_char('l'));
-        CHECK(pacif::parsing::is_ordinary_char('m'));
-        CHECK(pacif::parsing::is_ordinary_char('n'));
-        CHECK(pacif::parsing::is_ordinary_char('o'));
-        CHECK(pacif::parsing::is_ordinary_char('p'));
-        CHECK(pacif::parsing::is_ordinary_char('q'));
-        CHECK(pacif::parsing::is_ordinary_char('r'));
-        CHECK(pacif::parsing::is_ordinary_char('s'));
-        CHECK(pacif::parsing::is_ordinary_char('t'));
-        CHECK(pacif::parsing::is_ordinary_char('u'));
-        CHECK(pacif::parsing::is_ordinary_char('v'));
-        CHECK(pacif::parsing::is_ordinary_char('w'));
-        CHECK(pacif::parsing::is_ordinary_char('x'));
-        CHECK(pacif::parsing::is_ordinary_char('y'));
-        CHECK(pacif::parsing::is_ordinary_char('z'));
-        CHECK(pacif::parsing::is_ordinary_char('{'));
-        CHECK(pacif::parsing::is_ordinary_char('|'));
-        CHECK(pacif::parsing::is_ordinary_char('}'));
-        CHECK(pacif::parsing::is_ordinary_char('~'));
+        const auto ORDINARY_CHARS = {
+            '!', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1',
+            '2', '3', '4', '5', '6', '7', '8', '9', ':', '<', '=', '>', '?',
+            '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+            'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+            'Z', '\\', '^', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+            'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+            'w', 'x', 'y', 'z', '{', '|', '}', '~'
+        };
+
+        for (auto c: ORDINARY_CHARS) {
+            CHECK(pacif::parsing::is_ordinary_char(c));
+            CHECK(pacif::parsing::is_non_blank_char(c));
+            CHECK_FALSE(pacif::parsing::is_whitespace(c));
+        }
     }
 
     SECTION("non blank char") {
-        // All the ordinary chars
-        CHECK(pacif::parsing::is_non_blank_char('!'));
-        CHECK(pacif::parsing::is_non_blank_char('%'));
-        CHECK(pacif::parsing::is_non_blank_char('&'));
-        CHECK(pacif::parsing::is_non_blank_char('('));
-        CHECK(pacif::parsing::is_non_blank_char(')'));
-        CHECK(pacif::parsing::is_non_blank_char('*'));
-        CHECK(pacif::parsing::is_non_blank_char('+'));
-        CHECK(pacif::parsing::is_non_blank_char(','));
-        CHECK(pacif::parsing::is_non_blank_char('-'));
-        CHECK(pacif::parsing::is_non_blank_char('.'));
-        CHECK(pacif::parsing::is_non_blank_char('/'));
-        CHECK(pacif::parsing::is_non_blank_char('0'));
-        CHECK(pacif::parsing::is_non_blank_char('1'));
-        CHECK(pacif::parsing::is_non_blank_char('2'));
-        CHECK(pacif::parsing::is_non_blank_char('3'));
-        CHECK(pacif::parsing::is_non_blank_char('4'));
-        CHECK(pacif::parsing::is_non_blank_char('5'));
-        CHECK(pacif::parsing::is_non_blank_char('6'));
-        CHECK(pacif::parsing::is_non_blank_char('7'));
-        CHECK(pacif::parsing::is_non_blank_char('8'));
-        CHECK(pacif::parsing::is_non_blank_char('9'));
-        CHECK(pacif::parsing::is_non_blank_char(':'));
-        CHECK(pacif::parsing::is_non_blank_char('<'));
-        CHECK(pacif::parsing::is_non_blank_char('='));
-        CHECK(pacif::parsing::is_non_blank_char('>'));
-        CHECK(pacif::parsing::is_non_blank_char('?'));
-        CHECK(pacif::parsing::is_non_blank_char('@'));
-        CHECK(pacif::parsing::is_non_blank_char('A'));
-        CHECK(pacif::parsing::is_non_blank_char('B'));
-        CHECK(pacif::parsing::is_non_blank_char('C'));
-        CHECK(pacif::parsing::is_non_blank_char('D'));
-        CHECK(pacif::parsing::is_non_blank_char('E'));
-        CHECK(pacif::parsing::is_non_blank_char('F'));
-        CHECK(pacif::parsing::is_non_blank_char('G'));
-        CHECK(pacif::parsing::is_non_blank_char('H'));
-        CHECK(pacif::parsing::is_non_blank_char('I'));
-        CHECK(pacif::parsing::is_non_blank_char('J'));
-        CHECK(pacif::parsing::is_non_blank_char('K'));
-        CHECK(pacif::parsing::is_non_blank_char('L'));
-        CHECK(pacif::parsing::is_non_blank_char('M'));
-        CHECK(pacif::parsing::is_non_blank_char('N'));
-        CHECK(pacif::parsing::is_non_blank_char('O'));
-        CHECK(pacif::parsing::is_non_blank_char('P'));
-        CHECK(pacif::parsing::is_non_blank_char('Q'));
-        CHECK(pacif::parsing::is_non_blank_char('R'));
-        CHECK(pacif::parsing::is_non_blank_char('S'));
-        CHECK(pacif::parsing::is_non_blank_char('T'));
-        CHECK(pacif::parsing::is_non_blank_char('U'));
-        CHECK(pacif::parsing::is_non_blank_char('V'));
-        CHECK(pacif::parsing::is_non_blank_char('W'));
-        CHECK(pacif::parsing::is_non_blank_char('X'));
-        CHECK(pacif::parsing::is_non_blank_char('Y'));
-        CHECK(pacif::parsing::is_non_blank_char('Z'));
-        CHECK(pacif::parsing::is_non_blank_char('\\'));
-        CHECK(pacif::parsing::is_non_blank_char('^'));
-        CHECK(pacif::parsing::is_non_blank_char('`'));
-        CHECK(pacif::parsing::is_non_blank_char('a'));
-        CHECK(pacif::parsing::is_non_blank_char('b'));
-        CHECK(pacif::parsing::is_non_blank_char('c'));
-        CHECK(pacif::parsing::is_non_blank_char('d'));
-        CHECK(pacif::parsing::is_non_blank_char('e'));
-        CHECK(pacif::parsing::is_non_blank_char('f'));
-        CHECK(pacif::parsing::is_non_blank_char('g'));
-        CHECK(pacif::parsing::is_non_blank_char('h'));
-        CHECK(pacif::parsing::is_non_blank_char('i'));
-        CHECK(pacif::parsing::is_non_blank_char('j'));
-        CHECK(pacif::parsing::is_non_blank_char('k'));
-        CHECK(pacif::parsing::is_non_blank_char('l'));
-        CHECK(pacif::parsing::is_non_blank_char('m'));
-        CHECK(pacif::parsing::is_non_blank_char('n'));
-        CHECK(pacif::parsing::is_non_blank_char('o'));
-        CHECK(pacif::parsing::is_non_blank_char('p'));
-        CHECK(pacif::parsing::is_non_blank_char('q'));
-        CHECK(pacif::parsing::is_non_blank_char('r'));
-        CHECK(pacif::parsing::is_non_blank_char('s'));
-        CHECK(pacif::parsing::is_non_blank_char('t'));
-        CHECK(pacif::parsing::is_non_blank_char('u'));
-        CHECK(pacif::parsing::is_non_blank_char('v'));
-        CHECK(pacif::parsing::is_non_blank_char('w'));
-        CHECK(pacif::parsing::is_non_blank_char('x'));
-        CHECK(pacif::parsing::is_non_blank_char('y'));
-        CHECK(pacif::parsing::is_non_blank_char('z'));
-        CHECK(pacif::parsing::is_non_blank_char('{'));
-        CHECK(pacif::parsing::is_non_blank_char('|'));
-        CHECK(pacif::parsing::is_non_blank_char('}'));
-        CHECK(pacif::parsing::is_non_blank_char('~'));
-        // Other non blank chars
-        CHECK(pacif::parsing::is_non_blank_char('"'));
-        CHECK(pacif::parsing::is_non_blank_char('\''));
-        CHECK(pacif::parsing::is_non_blank_char('$'));
-        CHECK(pacif::parsing::is_non_blank_char('#'));
-        CHECK(pacif::parsing::is_non_blank_char('_'));
-        CHECK(pacif::parsing::is_non_blank_char(';'));
-        CHECK(pacif::parsing::is_non_blank_char('['));
-        CHECK(pacif::parsing::is_non_blank_char(']'));
+        const auto NON_BLANK_CHARS = {
+            '!', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1',
+            '2', '3', '4', '5', '6', '7', '8', '9', ':', '<', '=', '>', '?',
+            '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+            'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+            'Z', '\\', '^', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+            'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+            'w', 'x', 'y', 'z', '{', '|',  '}', '~', '"', '\'', '$', '#', '_',
+            ';', '[', ']'
+        };
+
+        for (auto c: NON_BLANK_CHARS) {
+            CHECK(pacif::parsing::is_non_blank_char(c));
+            CHECK_FALSE(pacif::parsing::is_whitespace(c));
+        }
     }
 
     SECTION("tags") {
@@ -199,7 +57,7 @@ TEST_CASE("CIF parsing") {
         };
 
         for (auto& tag: TAGS) {
-            CHECK(pacif::parsing::is_tag_name(tag.begin(), tag.end()));
+            CHECK(pacif::parsing::is_tag_name(tag));
         }
 
         std::vector<std::string> NOT_TAGS = {
@@ -210,7 +68,123 @@ TEST_CASE("CIF parsing") {
         };
 
         for (auto& tag: NOT_TAGS) {
-            CHECK_FALSE(pacif::parsing::is_tag_name(tag.begin(), tag.end()));
+            CHECK_FALSE(pacif::parsing::is_tag_name(tag));
         }
+    }
+}
+
+TEST_CASE("tokenizer") {
+    using namespace pacif::parsing;
+
+    SECTION("keywords") {
+        auto token = parser("loop_").next();
+        CHECK(token.get_kind() == token::Loop);
+
+        token = parser("stop_").next();
+        CHECK(token.get_kind() == token::Stop);
+
+        token = parser("global_").next();
+        CHECK(token.get_kind() == token::Global);
+
+        token = parser("data_56").next();
+        CHECK(token.get_kind() == token::Data);
+        CHECK(token.as_string() == "56");
+
+        token = parser("save_56").next();
+        CHECK(token.get_kind() == token::Save);
+        CHECK(token.as_string() == "56");
+    }
+
+    SECTION("strings") {
+        auto token = parser("string").next();
+        CHECK(token.get_kind() == token::String);
+        CHECK(token.as_string() == "string");
+
+        token = parser("'string'").next();
+        CHECK(token.get_kind() == token::String);
+        CHECK(token.as_string() == "string");
+
+        token = parser("'string' not this one").next();
+        CHECK(token.get_kind() == token::String);
+        CHECK(token.as_string() == "string");
+
+        token = parser("'string'but this one'").next();
+        CHECK(token.get_kind() == token::String);
+        CHECK(token.as_string() == "string'but this one");
+
+        token = parser("\"string\"").next();
+        CHECK(token.get_kind() == token::String);
+        CHECK(token.as_string() == "string");
+
+        token = parser("\"string\" not here").next();
+        CHECK(token.get_kind() == token::String);
+        CHECK(token.as_string() == "string");
+
+        token = parser("\"string\"but here\"").next();
+        CHECK(token.get_kind() == token::String);
+        CHECK(token.as_string() == "string\"but here");
+    }
+
+    SECTION("integers") {
+        auto token = parser("42").next();
+        CHECK(token.get_kind() == token::Integer);
+        CHECK(token.as_integer() == 42);
+
+        token = parser("-33").next();
+        CHECK(token.get_kind() == token::Integer);
+        CHECK(token.as_integer() == -33);
+
+        token = parser("+7833").next();
+        CHECK(token.get_kind() == token::Integer);
+        CHECK(token.as_integer() == 7833);
+
+        token = parser("42(4)").next();
+        CHECK(token.get_kind() == token::Integer);
+        CHECK(token.as_integer() == 424);
+
+        token = parser("42(4)4").next();
+        CHECK(token.get_kind() == token::String);
+        CHECK(token.as_string() == "42(4)4");
+
+        token = parser("9223372036854775807").next();
+        CHECK(token.get_kind() == token::Integer);
+        CHECK(token.as_integer() == 9223372036854775807);
+
+        CHECK_THROWS_AS(parser("9223372036854775808").next(), pacif::error);
+    }
+
+    SECTION("reals") {
+        auto token = parser("42.5").next();
+        CHECK(token.get_kind() == token::Real);
+        CHECK(token.as_real() == 42.5);
+
+        token = parser("42.5(3)").next();
+        CHECK(token.get_kind() == token::Real);
+        CHECK(token.as_real() == 42.53);
+
+        token = parser("42.").next();
+        CHECK(token.get_kind() == token::Real);
+        CHECK(token.as_real() == 42.);
+
+        token = parser(".42").next();
+        CHECK(token.get_kind() == token::Real);
+        CHECK(token.as_real() == .42);
+
+
+        token = parser("42e6").next();
+        CHECK(token.get_kind() == token::Real);
+        CHECK(token.as_real() == 42e6);
+
+        token = parser("42e-8").next();
+        CHECK(token.get_kind() == token::Real);
+        CHECK(token.as_real() == 42e-8);
+
+        token = parser("-25.5").next();
+        CHECK(token.get_kind() == token::Real);
+        CHECK(token.as_real() == -25.5);
+
+        token = parser("+67.9").next();
+        CHECK(token.get_kind() == token::Real);
+        CHECK(token.as_real() == +67.9);
     }
 }
