67f9ed11cc707bb68aa814f602d4d8facb6ac23c
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2019-07-10 15:59:50 +0200

    Refactor a basic_data class out of the data class

--- tests/data.cpp
+++ tests/data.cpp
@@ -2,26 +2,28 @@
 #include "cifxx/data.hpp"
 using namespace cifxx;
 
-TEST_CASE("data class") {
-    auto data = cifxx::data("this_is_data");
+TEST_CASE("basic_data class") {
+    auto data = cifxx::basic_data();
 
+    CHECK(data.empty());
+    CHECK(data.size() == 0);
     CHECK(data.find("_foo") == data.end());
 
-    data.insert("_float", 32);
+    data.emplace("_float", 32);
     CHECK(data.find("_float") != data.end());
     CHECK(data.find("_float")->second.is_number());
     CHECK(data.find("_float")->second.as_number() == 32);
     CHECK(data.get("_float").is_number());
     CHECK(data.get("_float").as_number() == 32);
 
-    data.insert("_string", "some more data");
+    data.emplace("_string", "some more data");
     CHECK(data.find("_string") != data.end());
     CHECK(data.find("_string")->second.is_string());
     CHECK(data.find("_string")->second.as_string() == "some more data");
     CHECK(data.get("_string").is_string());
     CHECK(data.get("_string").as_string() == "some more data");
 
-    data.insert("_vector", vector_t{value("here"), value(22)});
+    data.emplace("_vector", vector_t{value("here"), value(22)});
     CHECK(data.find("_vector") != data.end());
     CHECK(data.find("_vector")->second.is_vector());
     auto vector = data.find("_vector")->second.as_vector();
@@ -36,11 +38,27 @@ TEST_CASE("data class") {
     CHECK(vector[1].is_number());
     CHECK(vector[1].as_number() == 22);
 
-    data.insert("_override", 32);
-    auto result = data.insert("_override", "32");
+    data.emplace("_override", 32);
+    auto result = data.emplace("_override", "32");
     CHECK(result.second == false);
 
     CHECK(data.find("_override") != data.end());
     CHECK(data.find("_override")->second.is_number());
     CHECK(data.find("_override")->second.as_number() == 32);
+
+    CHECK_FALSE(data.empty());
+    CHECK(data.size() == 4);
+}
+
+
+TEST_CASE("data class") {
+    auto data = cifxx::data("this_is_data");
+    CHECK(data.name() == "this_is_data");
+
+    CHECK(data.find("_foo") == data.end());
+
+    data.emplace("_float", 32);
+    CHECK(data.find("_float") != data.end());
+    CHECK(data.find("_float")->second.is_number());
+    CHECK(data.find("_float")->second.as_number() == 32);
 }
